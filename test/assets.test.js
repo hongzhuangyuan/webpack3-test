d9bbd9079bcc63419363953f94e930a3336fe121
holi-java <holi-java@163.com>
2017-08-22 23:27:38 +0800

    use inline loaders

--- test/assets.test.js
+++ test/assets.test.js
@@ -1,5 +1,16 @@
 describe("assert", ()=> {
   
+  it("inline mode", done =>{
+
+    compile({entry: './inline.js'}).catch(done.fail).then(files => {
+      expect(files).toHaveLength(1)
+      expect(files.main).toEqual(expect.stringContaining("./main.css"))
+      expect(files.main).toEqual(expect.stringContaining("color: red;"))
+      done()
+    })
+
+  })
+
   it("css will be bundled", done => { 
     let module  = {
       rules: [
---
+++ test/data/inline.js
@@ -0,0 +1,1 @@
+import "style-loader!css-loader!./assets/main.css"
