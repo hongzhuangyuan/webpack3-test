2a008058af9c120a53252778304b5cb6f7a045c0
holi-java <holi-java@163.com>
2017-08-25 02:20:00 +0800

    test generate inlined source map in development mode

---
+++ test/dev.test.js
@@ -0,0 +1,18 @@
+/*global compile*/
+describe("development", () => {
+  it("generate sourceMapURL for the module with .js extension", done => {
+    compile({ entry: "./deps.js", devtool: "inline-source-map", output: { filename: "[name].js" } })
+      .catch(done.fail)
+      .then(files => {
+        expect(files["main.js"]).toMatch(/sourceMappingURL/gm)
+        done()
+      })
+  })
+
+  it("doesn't generate sourceMapURL for the module without .js | .css extension", done => {
+    compile({ entry: "./deps.js", devtool: "inline-source-map" }).catch(done.fail).then(files => {
+      expect(files.main).not.toMatch(/sourceMappingURL/gm)
+      done()
+    })
+  })
+})
