9998ffab44ad99a01548e91bfa3eaaf31f52be86
holi-java <holi-java@163.com>
2017-08-25 13:41:16 +0800

    make NODE_ENV to 'production'

--- test/global.test.js
+++ test/global.test.js
@@ -1,4 +1,8 @@
 describe("global", () => {
+  it("NODE_ENV", () => {
+    expect(process.env.NODE_ENV).toBe("production")
+  })
+
   it("__dirname", () => {
     expect(__dirname).toMatch(/\/test$/)
     expect(require("./data/dirname").__dirname).toMatch(/\/test\/data$/)
